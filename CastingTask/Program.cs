b47716d4853eae7548aaa0e67371c5c6d45141b8
Mirgubad <migubad@code.edu.az>
2022-06-03 00:27:01 +0400

    Add project files.

---
+++ CastingTask/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CastingTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataPrint(2.32);
+
+
+        }
+
+        public static void DataPrint(object data)
+        {
+            Console.WriteLine(data);
+        }
+    }
+
+
+
+
+
+}
